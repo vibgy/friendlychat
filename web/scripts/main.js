8ceceb88d59e6be7634251dca9b583e8dcc75e86
Gaurav Nolkha <gaurav.nolkha@intel.com>
2017-05-08 19:33:18 -0700

    added code for presence

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -194,6 +194,8 @@ FriendlyChat.prototype.onAuthStateChanged = function(user) {
 
     // We save the Firebase Messaging Device token and enable notifications.
     this.saveMessagingDeviceToken();
+
+    this.managePresence();
   } else { // User is signed out!
     // Hide user's profile and sign-out button.
     this.userName.setAttribute('hidden', 'true');
@@ -318,6 +320,36 @@ FriendlyChat.prototype.checkSetup = function() {
         'Make sure you go through the codelab setup instructions and make ' +
         'sure you are running the codelab using `firebase serve`');
   }
+};
+
+FriendlyChat.prototype.managePresence = function() {
+
+var myConnectionsRef = firebase.database().ref('users/joe/connections');
+
+// stores the timestamp of my last disconnect (the last time I was seen online)
+var lastOnlineRef = firebase.database().ref('users/joe/lastOnline');
+
+var connectedRef = firebase.database().ref('.info/connected');
+connectedRef.on('value', function(snap) {
+  if (snap.val() === true) {
+    console.log("connecting...");
+    // We're connected (or reconnected)! Do anything here that should happen only if online (or on reconnect)
+
+    // add this device to my connections list
+    // this value could contain info about the device or a timestamp too
+    var con = myConnectionsRef.push(true);
+
+    // when I disconnect, remove this device
+    con.onDisconnect().remove();
+
+    // when I disconnect, update the last time I was seen online
+    lastOnlineRef.onDisconnect().set(firebase.database.ServerValue.TIMESTAMP);
+  } else {
+    console.log("disconnecting...");
+  }
+
+});
+
 };
 
 window.onload = function() {
